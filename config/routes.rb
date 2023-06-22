fa58b60dcbe370284328fcf55117533144c2b888
Ruiyot Abby <ruiyotabby@gmail.com>
2023-06-22 17:02:00 +0300

    Allow saving of mesages to db

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'hangouts/index'
   devise_for :users
+  resources :messages, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
